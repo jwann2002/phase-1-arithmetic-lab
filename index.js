de786f0837f605a735dceb470a70b29852cb2003
Jwan <joan.kareem999@gmail.com>
2024-03-05 13:01:20 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod); // Output: 4
+let numbers = [5, 10, 15, 20];
+let max = Math.max(...numbers);
+
+console.log(max); // Output: 20
